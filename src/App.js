f1f75490b4a0e753d0c40bc430f4d9fb10fa8775
Jonathan Bijl <sannoayo@gmail.com>
2023-07-13 21:05:39 -0400

    Iteration 1 done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
+import React, { useState } from 'react';
+
+const countriesData = require('./countries.json');
 
 function App() {
+  const [countries, setCountries] = useState(countriesData);
+
   return <div className="App"></div>;
 }
+
+
 export default App;
